162c87c8d5db978fca33e784dc5bc548bc8296f7
= <=>
2025-03-31 21:09:05 +0300

    проработка логики авторизации, регистрации и выхода из аккаунта

--- react-diplom/src/components/sing-in/SingIn.jsx
+++ react-diplom/src/components/sing-in/SingIn.jsx
@@ -7,24 +7,17 @@ import Footer from '../footer/Footer';
 import Header from '../header/Header';
 
 export default function SingIn() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     async function UserSingIn() {
-        const password = document.querySelector('.password-input').value;
-        const email = document.querySelector('.email-input').value;
-
-        const singInWindow = document.querySelector('.sing-in-window-container'); // временно(возможно)
-        const adminPageBtn = document.querySelector('.admin-page-button'); // временно(возможно)
-        const logOutButton = document.querySelector('.log-out-button'); // временно(возможно)
-
-        // для проверки, отслеживания вводимых данных
-        console.log('Email:', email, 'Password:', password);
-
         try {
             const data = await fetch('http://localhost:3000/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify({ password, email }),
+                body: JSON.stringify({ email, password }),
             })
             .then(response => response.json());
 
@@ -33,30 +26,18 @@ export default function SingIn() {
             window.localStorage.setItem('email', data.email);
             window.localStorage.setItem('role', data.role);
 
-            // админ или нет? обработка // временно
-            if (data.role === 'admin') {
-                adminPageBtn.style.display = 'block';
-                alert('Вы вошли как администратор');
-            } else if (data.role === 'user'){
-                adminPageBtn.style.display = 'none';
-                alert('Добро пожаловать !');
-            } else {
-                alert('Неверный логин или пароль');
-            };
-            
-            // для показа/скрытия окна входа при нажатии
-            const token = window.localStorage.getItem('token');
-            if (token && token !== 'undefined'){          
-                singInWindow.style.display = 'none';
-                logOutButton.style.display = 'block';
-            } else {
-                console.log('Ошибка авторизации');
-            }
-
             console.log(data);
         } catch (error) {
             console.error('Ошибка при выполнении запроса:', error);
         }
+    }
+
+    const logOut = () => {
+        window.localStorage.removeItem('token');
+        window.localStorage.removeItem('email');
+        window.localStorage.removeItem('role');
+    
+        alert('Вы вышли из аккаунта');
     }
         
     return(
@@ -66,14 +47,14 @@ export default function SingIn() {
                 <div className="main-container-singin">
                     <div className="sing-in-window-container">
                         <div className="sing-in-window">
-                            <input type="text" className="password-input" placeholder="Введите пароль" />
-                            <input type="email" className="email-input" placeholder="Введите адрес электронной почты" />
+                            <input type="email" className="email-input" placeholder="Введите адрес электронной почты" onChange={e => setEmail(e.target.value)}/>
+                            <input type="text" className="password-input" placeholder="Введите пароль" onChange={e => setPassword(e.target.value)}/>
                             <button type="button" className="sing-in-button" onClick={UserSingIn}>Войти</button>
                         </div>
                     </div>
                     <div className="log-out-window-container">
                         <div className="log-out-window">
-                            <button type="button" className="log-out-button">Выйти из аккаунта</button>
+                            <button type="button" className="log-out-button" onClick={logOut}>Выйти из аккаунта</button>
                         </div>
                     </div>
                 </div>
--- react-diplom/src/components/sing-up/SingUp.jsx
+++ react-diplom/src/components/sing-up/SingUp.jsx
@@ -7,20 +7,12 @@ import Footer from '../footer/Footer';
 import Header from '../header/Header';
 
 export default function SingUp() {
-    const [userName, setUserName] = useState('');
-    const [userSurname, setUserSurname] = useState('');
-    const [userPassword, setUserpassword] = useState('');
-    const [userEmail, setUserEmail] = useState('');
+    const [name, setUserName] = useState('');
+    const [surname, setUserSurname] = useState('');
+    const [password, setUserPassword] = useState('');
+    const [email, setUserEmail] = useState('');
 
-    const user = {
-        name: '',
-        surname: '',
-        password: '',
-        email: ''
-    }
     async function UserSingUp() {
-
-    
         const response = await fetch('http://localhost:3000/register', {
             method: 'POST',
             headers: {
@@ -30,7 +22,6 @@ export default function SingUp() {
         });
     
         const data = await response.json();
-        data === true ? alert('Вы успешно зарегистрировались') : alert('Ошибка при регистрации');
 
         console.log(data);
     }
@@ -42,10 +33,10 @@ export default function SingUp() {
                 <div className="main-container-singup">
                     <div className="sing-up-window-container">
                         <div className="sing-up-window">
-                            <input type="text" className="name-input" placeholder="Введите имя" autoComplete="off" />
-                            <input type="text" className="surname-input" placeholder="Введите фамилию" autoComplete="off" />
-                            <input type="text" className="email-input" placeholder="Введите адрес электронной почты" autoComplete="off" />
-                            <input type="text" className="password-input" placeholder="Придумайте пароль" autoComplete="off" />
+                            <input type="text" className="name-input" placeholder="Введите имя" autoComplete="off" onChange={e => setUserName(e.target.value)}/>
+                            <input type="text" className="surname-input" placeholder="Введите фамилию" autoComplete="off" onChange={e => setUserSurname(e.target.value)}/>
+                            <input type="text" className="email-input" placeholder="Введите адрес электронной почты" autoComplete="off" onChange={e => setUserEmail(e.target.value)}/>
+                            <input type="text" className="password-input" placeholder="Придумайте пароль" autoComplete="off" onChange={e => setUserPassword(e.target.value)}/>
                             <button type="button" className="sing-up-button" onClick={UserSingUp}>Зарегистрироваться</button>
                         </div>
                     </div>
