12f5c23d10ec2c44c414850287db964a2d5f6b04
= <=>
2025-05-08 15:09:15 +0300

    работа над стилями страницы администратора

--- react-diplom/src/components/admin-page/components/add-engine/AddEngine.jsx
+++ react-diplom/src/components/admin-page/components/add-engine/AddEngine.jsx
@@ -33,50 +33,50 @@ export default function AddEngine(){
                 <div className="engine-parameters-container">
                     <h2>Добавление двигателя</h2>
                     <div className="engine-parameter serial-name">
-                        <p>пример: ej20, 4g18</p>
+                        <p>Пример: ej20, 4g18</p>
                         <input type="text" className="serial-number-input" placeholder="Серийный номер двигателя:" 
                         onChange={e => setEngineSerialName(e.target.value)}
                         />
                     </div>
                     <div className="engine-parameter engine-size">
-                        <p>пример: 1.6, 2.0</p>
-                        <input type="text" className="engine-size-input" placeholder="Объем двигателя:" 
+                        <p>Пример: 1.6, 2.0</p>
+                        <input type="text" className="engine-size-input" placeholder="Объем :" 
                         onChange={e => SetEngineSize(e.target.value)}
                         />
                     </div>
                     <div className="engine-parameter engine-type">
-                        <p>пример: inline, rotary</p>
-                        <input type="text" className="engine-type-input" placeholder="Тип двигателя двигателя:" 
+                        <p>Пример: inline, rotary</p>
+                        <input type="text" className="engine-type-input" placeholder="Тип :" 
                         onChange={e => SetEngineType(e.target.value)}
                         />
                     </div>
                     <div className="engine-parameter engine-nano">
-                        <p>пример: 320nm, 240nm</p>
-                        <input type="text" className="engine-nano-input" placeholder="Крутящий момент двигателя:" 
+                        <p>Пример: 320nm, 240nm</p>
+                        <input type="text" className="engine-nano-input" placeholder="Крутящий момент :" 
                         onChange={e => SetEngineNano(e.target.value)}
                         />
                     </div>
                     <div className="engine-parameter engine-horse-power">
-                        <p>пример: 123hp, 240лс</p>
-                        <input type="text" className="engine-horse-power-input" placeholder="Мощность л.с (лошадиные силы) двигателя:" 
+                        <p>Пример: 123hp, 240лс</p>
+                        <input type="text" className="engine-horse-power-input" placeholder="Мощность л.с (лошадиные силы) :" 
                         onChange={e => SetEngineHorsePower(e.target.value)}
                         />
                     </div>
                     <div className="engine-parameter engine-expenditure-city">
-                        <p>пример: 6.9, 7.2</p>
-                        <input type="text" className="engine-expenditure-city-input" placeholder="Расход топлива двигателя (по городу):" 
+                        <p>Пример: 6.9, 7.2</p>
+                        <input type="text" className="engine-expenditure-city-input" placeholder="Расход топлива (по городу):" 
                         onChange={e => SetEngineExpenditureCity(e.target.value)}
                         />
                     </div>
                     <div className="engine-parameter engine-expenditure-track">
-                        <p>пример: 5.4, 6.8</p>
-                        <input type="text" className="engine-expenditure-track-input" placeholder="Расход топлива двигателя (по трассе):" 
+                        <p>Пример: 5.4, 6.8</p>
+                        <input type="text" className="engine-expenditure-track-input" placeholder="Расход топлива (по трассе):" 
                         onChange={e => SetEngineExpenditureTrack(e.target.value)}
                         />
                     </div>
                     <div className="engine-parameter camshaft-system">
-                        <p>пример: dohc, vtek</p>
-                        <input type="text" className="camshaft-system-input" placeholder="Система распредвалов двигатея:" 
+                        <p>Пример: dohc, vtek</p>
+                        <input type="text" className="camshaft-system-input" placeholder="Система распредвалов :" 
                         onChange={e => SetEngineCamshaftSystem(e.target.value)}
                         />
                     </div>
