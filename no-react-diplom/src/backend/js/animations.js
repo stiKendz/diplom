2a1831e2f9217ab65a248c6e764150184563d9f7
= <=>
2025-07-04 09:59:09 +0300

    Диплом сдан. Папка no-react-diplom удалена.

--- no-react-diplom/src/backend/js/animations.js
+++
@@ -1,9 +0,0 @@
-const focusedDivFilter = document.querySelectorAll('.filter-item');
-
-if (focusedDivFilter.length > 0) {
-    for (let i = 0; i < focusedDivFilter.length; i++) {
-        focusedDivFilter[i].addEventListener('click', () => {
-            focusedDivFilter[i].classList.toggle('select');
-        });
-    };
-};
